efd94bf73b22a96940f521fc59fe2bf8a6631dc5
Pamela Argentino <argentinopam@hotmail.com>
2017-01-19 10:49:40 -0200

    Ongoing dynamic import of components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+// TODO this method DOES NOT SCALE - implement automatic exports
+// import RNFS from 'react-native-fs';
+
+import Button from './Button';
+import Card from './Card';
+import CardSection from './CardSection';
+import Header from './Header';
+
+export {
+    Button,
+    Card,
+    CardSection,
+    Header
+};
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
-import dummy from '../../components';
+import { Header } from '../../components';
 
 export default class App extends Component {
   render() {
     return (
-        <View style={styles.container}>
-          <Text>Hey I guess that this works!</Text>
-        </View>
+        <Header headerText="Authentication" />
     );
   }
 }
